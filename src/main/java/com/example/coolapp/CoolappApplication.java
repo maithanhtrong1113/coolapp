c1130d40ee37b5733d714ab7400c28f19f3cbba0
maithanhtrong1113 <maithanhtrong1113@gmail.com>
2023-07-11 18:27:18 +0700

    Create OBJECT use JPA entityManger

--- src/main/java/com/example/coolapp/CoolappApplication.java
+++ src/main/java/com/example/coolapp/CoolappApplication.java
@@ -1,5 +1,7 @@
 package com.example.coolapp;
 
+import com.example.coolapp.dao.StudentDAO;
+import com.example.coolapp.entity.Student;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -14,9 +16,19 @@ public class CoolappApplication {
     }
 
     @Bean
-    public CommandLineRunner commandLineRunner(String[] args) {
+    public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
         return runner -> {
-            System.out.println("hello");
+            createStudent(studentDAO);
         };
+    }
+
+    private void createStudent(StudentDAO studentDAO) {
+
+//        Alter table  student_tracker.student auto_increment=2000; thay đổi index bắt đầu =2000
+        Student student1= new Student("Thanh","Trong","maithanhtrong1113@gmail.com");
+        Student student2= new Student("Mai","Trong","maithanhtrong1113@gmail.com");
+
+        studentDAO.save(student1);
+        studentDAO.save(student2);
     }
 }
---
+++ src/main/java/com/example/coolapp/dao/StudentDAO.java
@@ -0,0 +1,7 @@
+package com.example.coolapp.dao;
+
+import com.example.coolapp.entity.Student;
+
+public interface StudentDAO {
+    void save (Student theStudent);
+}
